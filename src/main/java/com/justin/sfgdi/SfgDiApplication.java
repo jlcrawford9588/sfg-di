d3c6e2a11a5ead4eacd09fcfe46100d214b30758
jlcrawford9588 <jlcrawford9588@gmail.ccom>
2020-04-01 23:49:38 -0500

    JC - added more implementations of GreetingService to be able to demonstrate the usage of @Qualifier with spring dependency injection.

--- src/main/java/com/justin/sfgdi/SfgDiApplication.java
+++ src/main/java/com/justin/sfgdi/SfgDiApplication.java
@@ -16,9 +16,9 @@ public class SfgDiApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
 
+		System.out.println("-----------Primary Bean----------");
 		MyController myController = (MyController) context.getBean("myController");
-		String greeting = myController.sayHello();
-		System.out.println(greeting);
+		System.out.println(myController.getGreeting());
 
 		System.out.println("-----------Property----------");
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) context.getBean("propertyInjectedController");
--- src/main/java/com/justin/sfgdi/controllers/MyController.java
+++ src/main/java/com/justin/sfgdi/controllers/MyController.java
@@ -1,13 +1,18 @@
 package com.justin.sfgdi.controllers;
 
+import com.justin.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
-    public String sayHello() {
-        System.out.println("Hello World!!");
+    private final GreetingService greetingService;
 
-        return "Hi Folks!";
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
     }
 }
