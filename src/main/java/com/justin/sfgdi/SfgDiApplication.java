d84ed7c5a18bd4d86bd04cf4b9dbde2f82ef2be7
jlcrawford9588 <jlcrawford9588@gmail.ccom>
2020-04-01 22:47:36 -0500

    JC - adding spring context example using a basic controller bean.

--- src/main/java/com/justin/sfgdi/SfgDiApplication.java
+++ src/main/java/com/justin/sfgdi/SfgDiApplication.java
@@ -1,13 +1,19 @@
 package com.justin.sfgdi;
 
+import com.justin.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
+
+		MyController myController = (MyController) context.getBean("myController");
+		String greeting = myController.sayHello();
+		System.out.println(greeting);
 	}
 
 }
